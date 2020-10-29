c5f02b1ba5676c338207ad399060dd4cbfdb5ddb
Jose Cubero <cuberojose@gmal.com>
2020-10-29 13:14:50 +0100

    removed explicit df.plot() call, fixes issue with poetry

--- src/covid19_dashboard/dash_main.py
+++ src/covid19_dashboard/dash_main.py
@@ -5,19 +5,24 @@ import dash_core_components as dcc
 import dash_html_components as html
 import plotly.express as px
 import pandas as pd
-pd.options.plotting.backend = "plotly"
+# pd.options.plotting.backend = "plotly"
 
 # local imports
 from .data_parser.covid_JHU import get_clean_covid_data
+# from data_parser.covid_JHU import get_clean_covid_data
+
+def foo():
+    print("in foo context.")
 
 def launch_server():
     external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
     app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-    df = get_clean_covid_data('confirmed').T
+    df = get_clean_covid_data('confirmed').T.reset_index()
+    print(df)
 
-    fig = df.plot( )
-    fig2 = fig
+    fig = px.scatter(df, x="Date", y="Germany")
+    # fig2 = fig
 
     app.layout = html.Div(children=[
         html.H1(children='Hello Dash'),
@@ -30,15 +35,18 @@ def launch_server():
             id='example-graph',
             figure=fig
         ),
-        dcc.Graph(
-            id='example-graph2',
-            figure=fig2
-        )
+        # dcc.Graph(
+        #     id='example-graph2',
+        #     figure=fig2
+        # )
     ])
     
     print("Starting server app")
-    app.run_server(debug=True)
+    app.run_server()
+    # fig.show()
     print("Closing server app")
 
 if __name__ == '__main__':
+    print ("in main context.")
     launch_server()
+
