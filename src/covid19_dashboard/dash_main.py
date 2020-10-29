501aed0b6fcd5c613563625e2d75b5a3a140a4d8
Jose Cubero <cuberojose@gmal.com>
2020-10-29 02:35:35 +0100

    removed global statements from dash_main.py

--- src/covid19_dashboard/dash_main.py
+++ src/covid19_dashboard/dash_main.py
@@ -10,33 +10,35 @@ pd.options.plotting.backend = "plotly"
 # local imports
 from .data_parser.covid_JHU import get_clean_covid_data
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-
-df = get_clean_covid_data('confirmed').T
-
-fig = df.plot( )
-fig2 = fig
-
-app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
-
-    html.Div(children='''
-        Dash: A web application framework for Python.
-    '''),
-
-    dcc.Graph(
-        id='example-graph',
-        figure=fig
-    ),
-    dcc.Graph(
-        id='example-graph2',
-        figure=fig2
-    )
-])
-
 def launch_server():
+    external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+    app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+    df = get_clean_covid_data('confirmed').T
+
+    fig = df.plot( )
+    fig2 = fig
+
+    app.layout = html.Div(children=[
+        html.H1(children='Hello Dash'),
+
+        html.Div(children='''
+            Dash: A web application framework for Python.
+        '''),
+
+        dcc.Graph(
+            id='example-graph',
+            figure=fig
+        ),
+        dcc.Graph(
+            id='example-graph2',
+            figure=fig2
+        )
+    ])
+    
+    print("Starting server app")
     app.run_server(debug=True)
+    print("Closing server app")
 
 if __name__ == '__main__':
     launch_server()
