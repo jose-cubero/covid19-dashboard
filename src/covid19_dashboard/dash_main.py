e7dd52b161c0ca239cfee801c4c9bf5294ff64c4
Jose Cubero <cuberojose@gmal.com>
2020-10-29 01:34:21 +0100

    enabled poetry launch_server

--- src/covid19_dashboard/dash_main.py
+++ src/covid19_dashboard/dash_main.py
@@ -1,8 +1,5 @@
 # -*- coding: utf-8 -*-
 
-# Run this app with `python app.py` and
-# visit http://127.0.0.1:8050/ in your web browser.
-
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
@@ -11,19 +8,14 @@ import pandas as pd
 pd.options.plotting.backend = "plotly"
 
 # local imports
-from data_parser.covid_JHU import get_clean_covid_data
+from .data_parser.covid_JHU import get_clean_covid_data
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 df = get_clean_covid_data('confirmed').T
-print(df)
 
-# see https://plotly.com/python/px-arguments/ for more options
-# fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
-# fig = px.scatter(df, x="City", y="Amount", color="Fruit", barmode="group")
-# fig = px.scatter(df, x="City", y="Amount", color="Fruit", barmode="group")
-fig = df.plot()
+fig = df.plot( )
 fig2 = fig
 
 app.layout = html.Div(children=[
