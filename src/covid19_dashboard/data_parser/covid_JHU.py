01f4493af36917e8197b4f9b9ce73c8cefa5e6bf
Jose Cubero <cuberojose@gmal.com>
2020-11-05 16:05:19 +0100

    pandas df generation, primary vars, done

--- src/covid19_dashboard/data_parser/covid_JHU.py
+++ src/covid19_dashboard/data_parser/covid_JHU.py
@@ -6,8 +6,11 @@ __version__ = '1.0.0'
 import pandas as pd
 from pathlib import Path
 
-# _debug_lib = True
-# _lib_path = str(Path(__file__).parent)
+# local modules
+from covid19_dashboard.data_parser.world_pop import get_world_pop
+
+_debug_lib = True
+_lib_path = str(Path(__file__).parent)
 
 def parse_timeseries_csv(data_set, country_list=None):
 
@@ -16,7 +19,6 @@ def parse_timeseries_csv(data_set, country_list=None):
         print("error, data_set" + data_set + "does not exist")
         exit(5)
 
-    # file_name = _lib_path+'/data/time_series_covid19_' + data_set + '_global.csv'
     JHU_url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/"
     JHU_url += 'time_series_covid19_' + data_set + '_global.csv'
     df = pd.read_csv(JHU_url, parse_dates=True)
@@ -69,18 +71,19 @@ def parse_timeseries_csv(data_set, country_list=None):
     # 2. Reset index to default (numerical) and fix axis' names
     df = df.rename_axis('Date', axis='index')
     df.reset_index(inplace=True)
-    # df = df.rename_axis('idx', axis='index')  # unnecessary, due to following melt
+    df = df.rename_axis('', axis='columns') 
 
     df['Date'] =  pd.to_datetime(df['Date'])
 
     # 3. Melt
+    var_name = data_set.capitalize()
     df = df.melt(id_vars=['Date'],
             var_name='Country',
-            value_name='Value')
+            value_name=var_name)
 
     # df is now in LONG format, with numerical index
     # HEADER
-    # idx, date, country, value
+    # idx, date, country, $var_name: [Confirmed | Deaths | Recovered]
 
     ##### Debug
     # if (_debug_lib):
@@ -90,20 +93,108 @@ def parse_timeseries_csv(data_set, country_list=None):
 
     return df
 
-def get_clean_covid_data():
-    orig_var_list = ['confirmed', 'deaths', 'recovered']
-    df_list = []
+def get_covid_data_primary():
 
-    for var in orig_var_list:
-        dfx = parse_timeseries_csv(var)
-        dfx['Var'] = var.capitalize()
-        df_list.append(dfx)
+    # Long Format
+    # orig_var_list = ['confirmed', 'deaths', 'recovered']
+    # df_list = []
 
-    return pd.concat(df_list, axis=0, ignore_index=True)
+    # for var in orig_var_list:
+    #     dfx = parse_timeseries_csv(var)
+    #     # dfx['Var'] = var.capitalize()
+    #     df_list.append(dfx)
+    # return pd.concat(df_list, axis=0, ignore_index=True)
+
+    # Wide Format
+
+    df_conf = parse_timeseries_csv('confirmed')
+    df_deat = parse_timeseries_csv('deaths')
+    df_rec = parse_timeseries_csv('recovered')
+
+    df_merged = pd.merge(df_conf, df_deat, on=['Country', 'Date'], copy=False)
+    df_merged = pd.merge(df_merged, df_rec, on=['Country', 'Date'], copy=False)
+
+    return df_merged
+
+
+# def create_flat_df(country_df, world_pop_df):
+#     reg_df = pd.merge(world_pop_df.loc[: , ["UN_Region"]], country_df, left_index=True, right_index=True, how='inner').groupby("UN_Region").sum()
+#     con_df = pd.merge(world_pop_df.loc[: , ["Continent"]], country_df, left_index=True, right_index=True, how='inner').groupby("Continent").sum()
+#     df_merged = pd.concat([country_df, reg_df, con_df], axis=0)
+#     return df_merged
+
+
+def get_covid_data_all():
+    df_covid_pri = get_covid_data_primary() # Mixed/Wide Format: (Country + Date) -> conf, deaths, rec
+    df_world_pop = get_world_pop()          # Long Format:       (Country) -> un_region, continent 
+
+    # 1. Merge with population df
+
+    # reg_df = pd.merge(world_population_df.loc[: , ["UN_Region"]], country_df, left_index=True, right_index=True, how='inner').groupby("UN_Region").sum()
+    # con_df = pd.merge(world_population_df.loc[: , ["Continent"]], country_df, left_index=True, right_index=True, how='inner').groupby("Continent").sum()
+    # df_merged = pd.concat([country_df, reg_df, con_df], axis=0)
+
+    # df_merged = pd.merge(df_world_pop.loc[: , ["UN_Region"]], country_df, left_index=True, right_index=True, how='inner')
+
+
+    print(df_covid_pri)
+    print(df_world_pop)
+    df_countries = pd.merge(df_covid_pri, df_world_pop,  on='Country', how='inner')
+
+    # 2. Generate Sum Rows for Continent & Region, 
+
+    # df_regions    = df_merged.groupby(['UN_Region', 'Date'], as_index=False).sum()
+
+    # debugcsv = _lib_path+'/debug/OOPS.csv'
+    # df_merged.to_csv(debugcsv)
+    # print(df_merged)
+
+    # TODO: Research on possible bug/issue with categorical
+    df_regions = df_countries.groupby(['UN_Region', 'Date'], as_index=False).agg(
+                        {
+                        'Population_2019': 'sum',
+                        'Confirmed': 'sum',
+                        'Deaths': 'sum',
+                        'Recovered': 'sum',
+                        'Continent' : 'first'
+                        })
+    
+    df_continents = df_countries.groupby(['Continent', 'Date'], as_index=False).agg(
+                        {'Population_2019': 'sum',
+                        'Confirmed': 'sum',
+                        'Deaths': 'sum',
+                        'Recovered': 'sum'
+                        })
+
+    # 3. Add new column entry type
+
+    df_countries['Entry_Type'] = "country"
+    df_countries.rename(columns={'Country': 'Location'}, inplace=True)
+
+    df_regions['Entry_Type'] = "un_region"
+    df_regions.rename(columns={'UN_Region': 'Location'}, inplace=True)
+
+    df_continents['Entry_Type'] = "continent"
+    df_continents.rename(columns={'Continent': 'Location'}, inplace=True)
+
+    # 4. Concat 
+    df = pd.concat([df_countries, df_regions, df_continents], ignore_index=True, copy=False)
+
+    debugcsv = _lib_path+'/debug/ALL.csv'
+    df.to_csv(debugcsv)
+
+    print(df)
+    return df
+
 
 if __name__ == '__main__':
 
-    df_full = get_clean_covid_data()
-    print(df_full)
+    get_covid_data_all()
+    
+    # regions1 = df_full.groupby(["UN_Region", 'Date', 'Var'], as_index=False, dropna=False).sum()
+    # print(regions1)
+
+    # regions2 = df_full.groupby(["UN_Region", 'Date', 'Var'], as_index=True).sum()
+    # print(regions2)
 
     exit()
---
+++ src/covid19_dashboard/data_parser/world_pop.py
@@ -0,0 +1,147 @@
+# Parse population python module
+__author__ = 'Jose Cubero'
+__version__ = '1.0.0'
+
+#import os.path
+import pandas as pd
+from pathlib import Path
+
+_debug_lib = True
+_lib_path = str(Path(__file__).parent)
+
+def get_continent_list():
+
+    UN_continent_list = ["Americas",
+                "Europe",
+                "Africa",
+                "Asia",
+                "Oceania",
+            ]
+    
+    return UN_continent_list
+
+def get_region_list():
+
+    UN_region_list = ["Northern America",
+                "Central America",
+                "Caribbean",
+                "South America",
+                "Northern Europe",
+                "Western Europe",
+                "Southern Europe",
+                "Eastern Europe",
+                "Northern Africa",
+                "Western Africa",
+                "Eastern Africa",
+                "Middle Africa",
+                "Southern Africa",
+                "Western Asia",
+                "Central Asia",
+                "Eastern Asia",
+                "Southern Asia",
+                "South-eastern Asia",
+                "Australia and New Zealand",
+                "Melanesia",
+#                "Polynesia",
+                "Micronesia"
+            ]
+    return UN_region_list
+
+# def get_country_list(region_list=[], continent_list=[]):
+
+#     df = world_population_df
+
+#     #Apply optional filters
+#     if(region_list!= []):
+#         df = df[ (df['UN_Region'].isin(region_list) )]
+#     elif(continent_list != []):
+#         df = df[ (df['Continent'].isin(continent_list) )]
+
+#     return df.index.tolist()
+
+
+def get_world_pop(country_list=[], region_list=[], continent_list=[]):
+
+    file_worldpop = _lib_path+'/data/world_pop_wikipedia.csv'
+    # CSV fields: Country_Area,Continent,UN_Region,Population_2018,Population_2019,Change
+
+    df = pd.read_csv(file_worldpop)
+    df = df.loc[:,['Country_Area', 'Continent', 'UN_Region', 'Population_2019']]
+    # df = df.rename(columns={'Country_Area': 'Country'}).set_index('Country')
+    df = df.rename(columns={'Country_Area': 'Country'})
+
+    cat_reg_type = pd.CategoricalDtype(categories=(get_region_list()), ordered=True)
+    df['UN_Region'] = df['UN_Region'].astype(cat_reg_type)
+
+    cat_cont_type = pd.CategoricalDtype(categories=(get_continent_list()), ordered=True)
+    df['Continent'] = df['Continent'].astype(cat_cont_type)
+
+    if (_debug_lib):
+        debugcsv = _lib_path+'/debug/clean_world_pop_all.csv'
+        df.sort_index().to_csv(debugcsv, columns=[], header=False)
+
+    #Apply optional filters
+    if (country_list != []):
+        df = df.loc[ country_list, :]
+    elif(region_list!= []):
+        df = df[ (df['UN_Region'].isin(region_list) )]
+    elif(continent_list!= []):
+        df = df[ (df['Continent'].isin(continent_list) )]
+
+    return df
+
+
+def get_extended_world_pop(cg_dict = {}, filter_list =[]):
+    df0 = get_world_pop()
+    df_country = df0.loc[:, ['Population_2019'] ].sort_index()
+    df_region = df0.groupby('UN_Region').sum()
+    df_continent = df0.groupby('Continent').sum()
+
+    df_out = pd.concat([df_country, df_region, df_continent])
+
+    # if (_debug_lib):
+    #     debugcsv = _lib_path+'/debug/clean_world_pop_extended.csv'
+    #     df_out.sort_index().to_csv(debugcsv, columns=[], header=False)
+
+    # Add custom groups
+    if (cg_dict != {}):
+        for cg in cg_dict:
+            df_out.loc[cg, :] = df_out.loc[ cg_dict[cg], :].sum(axis=0)
+
+    #Apply optional filters
+    if (filter_list != []):
+        df_out = df_out.loc[filter_list, :]
+
+    return df_out
+
+def get_extended_wide():
+    df_full = get_world_pop()
+    
+    # regions = df_full.groupby(["UN_Region"], as_index=False, group_keys=False).sum()
+    regions = df_full.groupby(["UN_Region"], as_index=False).agg({'Population_2019': 'sum', 'Continent': 'first'})
+
+    return pd.concat([df_full,  regions], ignore_index=True, copy=False)
+
+
+# def get_region_country_dict():
+
+#     dictX = {}
+#     grouped_data = world_population_df.groupby(by= "UN_Region")
+    
+#     # Find countries with more infections, per region
+#     for region, dfx in grouped_data:
+#         dictX[region] = dfx.index.tolist()
+#     return dictX
+
+# world_population_df = get_world_pop()
+# UN_region_dict = get_region_country_dict()
+
+# TODO: create a proper lib, with read-only internal objects- getter/setter functions.
+
+if __name__ == '__main__':
+
+    df = get_extended_wide()
+
+    print(df)
+
+    exit()
