bb3794991762be2a49c08f543feda4e0991c2c67
Jose Cubero <cuberojose@gmal.com>
2020-11-05 21:02:11 +0100

    first gui sketch

--- src/covid19_dashboard/dash_main.py
+++ src/covid19_dashboard/dash_main.py
@@ -16,27 +16,44 @@ def launch_server():
     external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
     app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-    df = jhu.parse_timeseries_csv('confirmed')
+    # df = jhu.parse_timeseries_csv('confirmed')
+    # df = jhu.get_covid_data_all().loc[:, ['Date', 'Location', 'Confirmed', 'Entry_Type']]
+    df = jhu.get_covid_data_all()
+    df = df[df['Entry_Type'] == 'un_region']
     # available_indicators = df['Indicator Name'].unique()
 
-    fig = px.line(df, x='Date', y='Value', color='Country')
+    # fig = px.area(df, x='Date', y=['Confirmed'], color='Location',title="Graph 0 tittle", facet_col='Continent')
+    fig = px.area(df, x='Date', y=['Confirmed', 'Deaths'], color='Location',title="Graph 0 tittle", facet_col='Continent')
 
     app.layout = html.Div(children=[
-        html.H1(children='Hello Dash'),
+        html.H1(children='COVID-19 Timelines'),
 
-        html.Div(children='''
-            Dash: A web application framework for Python.
+        html.H4(children='''
+            A tool for data analysis. THIS TEXT CONTINUES HERE...
         '''),
 
+        dcc.Dropdown(
+            options=[
+                {'label': 'New York City', 'value': 'NYC'},
+                {'label': 'Montréal', 'value': 'MTL'},
+                {'label': 'San Francisco', 'value': 'SF'}
+            ],
+            value='MTL'
+        ), 
         dcc.Graph(
-            id='example-graph',
+            id='graph1',
             figure=fig
-        ),
-        # dcc.Graph(
-        #     id='example-graph2',
-        #     figure=fig2
-        # )
-    ])
+        )
+           
+            # dcc.Graph(
+            #     id='example-graph2',
+            #     figure=fig2
+            # )
+        ],
+        # style={'width': '50%','padding-left':'25%', 'padding-right':'25%'},
+        # style={"display": "inline-block"}
+#         style={"display": "inline-block", "grid-template-columns": "20% 80%", }
+    )
     
     print("Starting server app")
     app.run_server(debug=True)
@@ -46,3 +63,19 @@ if __name__ == '__main__':
     print ("in main context.")
     launch_server()
 
+##### Saved forlater
+        # children=[dcc.Graph(
+        #     id='histogram-graph',
+        #     figure={
+        #         'data': [{
+        #             'x': df['complaint_type'],
+        #             'text': df['complaint_type'],
+        #             'customdata': df['key'],
+        #             'name': '311 Calls by region.',
+        #             'type': 'histogram'
+        #         }],
+        #         'layout': {
+        #             'title': 'NYC 311 Calls category.',
+        #             'height': 500,
+        #             'padding': 150
+        #         }
